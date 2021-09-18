766e03efbe324815234fbf52a589b682bae3f782
k_yoshi <[deepdish.mind@gmail.com]>
2021-09-19 00:22:43 +0900

    棒グラフの出力.cpp

---
+++ APG4b/1.10.while文/棒グラフの出力.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+ 
+  // ここにプログラムを追記
+  int i,j;
+  i = 0;
+  j = 0;
+  
+  cout << "A:";
+  while(i < A){
+      cout << "]";
+      i++;
+  }
+  cout << endl;
+
+  cout << "B:";
+  while(j < B){
+      cout << "]";
+      j++;
+  }
+  cout << endl;
+}
